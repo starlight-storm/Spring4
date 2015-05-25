91cdd9bbc78ec435983eff24aa00bf39873bb029
hasegawadanna <hase@starlight-storm.com>
2015-05-25 16:09:20 +0900

    chapter3

---
+++ aop-config/src/main/java/sample/ProductSampleRun.java
@@ -0,0 +1,24 @@
+package sample;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import sample.di.business.domain.Product;
+import sample.di.business.service.ProductService;
+
+public class ProductSampleRun {
+
+    public static void main(String[] args) {
+        ProductSampleRun productSampleRun = new ProductSampleRun();
+        productSampleRun.execute();
+    }
+
+    @SuppressWarnings("resource")
+	public void execute() {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "/sample/config/applicationContext.xml");
+        ProductService productService = ctx.getBean(ProductService.class);
+        Product product = productService.findProduct();
+        System.out.println(product);
+    }
+}
